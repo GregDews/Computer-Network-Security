1903aac49bfef20602ba7402bd25101f6ceb9abd
Greg Dews <gdews@msudenver.edu>
2019-08-30 15:45:32 -0600

    algorithm written, needs testing

--- Encryption.java
+++ Encryption.java
@@ -10,7 +10,7 @@
  *          Will have system out dumped into text file via cs3750a server command
  * 
  *      To-Do
- *  encrypt() private method - create routine for encryption
+ *  Test algorithm
  *  displayResults() private method - create printf statements
 */
 import java.util.Scanner;
@@ -21,52 +21,57 @@ public class Encryption {
     Scanner kb;
 
     // Algorithm Constants
-    int deltaOne = 0x11111111;
-    int deltaTwo = 0x22222222;
+    int delta[] = new int[3];
 
-    // Display Variables
-    int l0, l1, l2, r0, r1, r2;
-    int cryptext;
+    // Encryption Variables
+    int[] L = new int[3], 
+          R = new int[3],
+          K = new int[4];
 
     public Encryption() {
         kb = new Scanner(System.in);
     }
 
     public void encrypt() {
-        int data = pullHexUInt();
-        int key = pullHexUInt();
-        cryptext = encrypt(data, key);
-
+        pullHexUInt(K, "K", 4);
+        pullHexUInt(L,"L", 1);
+        pullHexUInt(R,"R", 1);
+        encryptTEA();
         displayResults();
     }
 
     // private encryption function data = 64-bit, key = 128-bit
-    // returns 0 on error
-    private int encrypt(int data, int key){
-        int scrambled = 0;
-        // Stub - see homework for bitwise meddling
-
-        return scrambled;
+    private void encryptTEA(){
+        for(int pass = 1; pass <= 2; pass++){
+            L[pass] = R[pass-1];
+            R[pass] = L[pass] + bigF(R[pass-1], K[pass-1], K[pass], delta[pass]); 
+        }
     }
 
-    // requests hex string input, returns unsigned int
-    private int pullHexUInt() {
-        Boolean retry;
-        int uint = 0;
-        System.out.println("Please input K[i] in Hex String (without '0x')");
-        do {
+    // subroutine function of encryption
+    private int bigF(int r, int k1, int k2, int d){
+        int result = 0;
+        result = ((r<<4) + k1) ^ ((r>>5) + k2) ^ (r + d);
+        return result;
+    }
 
+    // requests hex string input, load int array with provided values
+    private void pullHexUInt(int[] arr, String letter, int length) {
+        Boolean retry; 
+        for(int i = 0; i < length; i++){
+        System.out.println("Please input "+ letter +
+                            "["+ i +"] in Hex String (without '0x')");
+        do {
             retry = false;
             try {
-                uint = Integer.parseUnsignedInt(kb.nextLine(),16);
+                arr[i] = Integer.parseUnsignedInt(kb.nextLine(),16);
             } catch (NumberFormatException e) {
                 System.out.println("That was not a valid Hex String \n" + "Please try again.");
                 retry = true;
             }
 
         } while (retry == true);
-
-        return uint;
+        }
     } // End of pullHexUInt
 
     // display formatted results
