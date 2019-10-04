2cc2f3df16d358c8dafb3d36d3b7194582326552
Greg Dews <gdews@msudenver.edu>
2019-10-03 23:26:09 -0600

    created key from secret key file

--- Sender.java
+++ Sender.java
@@ -1,133 +1,109 @@
 
 /*
-Greg Dews
+Greg Dews &
+Jeff Walto
 To-Do
-    still sifting through provided code. Modyfing to match needs
-    need to create a loop to handle buffer issues and array size matching
-
-sender 
-    enKx-(SHA256(M))
-    append message to that
-    enkxy
-
+    AES encryption on (RSA-(SHA256(M))//M) aka message.ds-msg -> message.aescipher
+    ((I know that this is not exactly secure, but for the sake of this class it works.))
+    test run, check that everything works.
 */
+
 import java.io.*;
-import java.util.Scanner;
+import java.math.BigInteger;
 import java.security.Key;
 import java.security.MessageDigest;
-import java.security.PublicKey;
 import java.security.PrivateKey;
 import java.security.SecureRandom;
 import java.security.KeyFactory;
-import java.security.spec.RSAPublicKeySpec;
-import java.security.spec.KeySpec;
 import java.security.spec.RSAPrivateKeySpec;
-import java.math.BigInteger;
+import java.util.Scanner;
 import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Sender {
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    // Get Ciphers/Hasher/keys
-    Cipher RSA = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    Cipher AES = Cipher.getInstance("AES/ECB/PKCS1Padding");
-    MessageDigest hasher = MessageDigest.getInstance("SHA-256");
-    PrivateKey privKey = readPrivKeyFromFile();
-    //Key secretKey = readSecretKeyFromFile();
-    SecureRandom random = new SecureRandom();
-    BufferedInputStream input;
+        // Get Ciphers/Hasher/keys
+        Cipher RSA = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        Cipher AES = Cipher.getInstance("AES/ECB/PKCS1Padding");
+        MessageDigest hasher = MessageDigest.getInstance("SHA-256");
+        PrivateKey privKey = readPrivKeyFromFile();
+        Key secretKey = readSecretKeyFromFile();
+        SecureRandom random = new SecureRandom();
+        BufferedInputStream input;
 
-    // initialize RSA
-    RSA.init(Cipher.ENCRYPT_MODE, privKey, random);
-    //AES.init(Cipher.ENCRYPT_MODE, secretKey, random);
+        // initialize RSA
+        RSA.init(Cipher.ENCRYPT_MODE, privKey, random);
+        AES.init(Cipher.ENCRYPT_MODE, secretKey, random);
 
-    // Get the file
-    System.out.println("Input the file path and name of the file we will encrypt.");
-    Scanner kb = new Scanner(System.in);
-    InputStream in = new FileInputStream(kb.nextLine());
-    input = new BufferedInputStream(in);
-    kb.close();
+        // Get the file
+        System.out.println("Input the file path and name of the file we will encrypt.");
+        Scanner kb = new Scanner(System.in);
+        File M = new File(kb.nextLine());
+        InputStream in = new FileInputStream(M);
+        input = new BufferedInputStream(in);
+        kb.close();
 
-    // Hash the file - 1028 byte increment
-    byte[] sha256;
-
-    while(input.available() > 1028){
-        hasher.update(input.readNBytes(1028));
-    }
-    if(input.available() > 0){
-        hasher.update(input.readAllBytes());
-    }
-    sha256 = hasher.digest();
-
-    // Display Hash Value
-    System.out.println("SHA-256 Value: ");
-    for (int i=0, j=0; i< sha256.length; i++, j++) {
-      System.out.format("%2X ", sha256[i]) ;
-      if (j >= 15) {
-        System.out.println("");
-        j=-1;
-      }
-    }
-    System.out.println("");
-
-    // append message to end of sha256 hash
-    try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-            new FileOutputStream("symmetric.key")))) {
-            writer.write("filler");
+        // Hash the file - 1028 byte increment
+        byte[] sha256;
+        while (input.available() > 1028) {
+            hasher.update(input.readNBytes(1028));
+        }
+        if (input.available() > 0) {
+            hasher.update(input.readAllBytes());
+        }
+        input.close();
+        in.close();
+        sha256 = hasher.digest();
+        try (FileOutputStream writer = new FileOutputStream("message.dd", true)) {
+            writer.write(sha256);
         }
 
-    /* V V V V V V V V V V V V V V TO-DO V V V V V V V V V V V V V V V V V
-    int block_size = RSA.getBlockSize();
-     byte[] cipherBlock = new byte[block_size];
-   
-
-    // finalize last block
-    byte[] cipherText = RSA.doFinal(input);
-
-    // display ciphertext byte info
-    System.out.println("cipherText: block size = " + cipher.getBlockSize());
-    for (int i=0, j=0; i<cipherText.length; i++, j++) {
-      System.out.format("%2X ", cipherText[i]) ;
-      if (j >= 15) {
-        System.out.println("");
-        j=-1;
-      }
-    }
-    System.out.println("");
-  }
-
-    // read input file - call
-
-    // read key parameters from a file and generate the public key
-    public static PublicKey readPubKeyFromFile() throws IOException {
-
-        InputStream in =
-                // Sender.class.getResourceAsStream(keyFileName);
-                new FileInputStream("./KeyGen/XRSAPublic.key");
-        ObjectInputStream oin = new ObjectInputStream(new BufferedInputStream(in));
-
-        try {
-            BigInteger m = (BigInteger) oin.readObject();
-            BigInteger e = (BigInteger) oin.readObject();
-
-            System.out.println(
-                    "Read from XRSAPublic.key: modulus = " + m.toString() + ", exponent = " + e.toString() + "\n");
-
-            RSAPublicKeySpec keySpec = new RSAPublicKeySpec(m, e);
-            KeyFactory factory = KeyFactory.getInstance("RSA");
-            PublicKey key = factory.generatePublic(keySpec);
-
-            return key;
-        } catch (Exception e) {
-            throw new RuntimeException("Spurious serialisation error", e);
-        } finally {
-            oin.close();
+        // Display Hash Value
+        System.out.println("SHA-256 Value: ");
+        for (int i = 0, j = 0; i < sha256.length; i++, j++) {
+            System.out.format("%2X ", sha256[i]);
+            if (j >= 15) {
+                System.out.println("");
+                j = -1;
+            }
         }
+        System.out.println("");
+
+        // RSA the hash value output message.ds-msg
+        byte[] cipherHash = RSA.doFinal(sha256);
+        try (FileOutputStream writer = new FileOutputStream("message.ds-msg", true)) {
+            writer.write(cipherHash);
+        }
+
+        // Display Digital Signature
+        System.out.println("Digital Signature: ");
+        for (int i = 0, j = 0; i < cipherHash.length; i++, j++) {
+            System.out.format("%2X ", cipherHash[i]);
+            if (j >= 15) {
+                System.out.println("");
+                j = -1;
+            }
+        }
+        System.out.println("");
+
+        // append message to end of RSA(SHA256(M)) - message.ds-msg
+        try (FileInputStream reader = new FileInputStream(M);
+                FileOutputStream writer = new FileOutputStream("message.ds-msg", true)) {
+            byte[] temp = new byte[1028];
+            while (reader.available() > 0) {
+                reader.read(temp);
+                writer.write(temp);
+            }
+        }
+
+//     V V V V V V V V V V V V V V TO-DO V V V V V V V V V V V V V V V V V
+
+        // encrypt DS//M with AES and store in message.aescipher
+        
+
     }
-*/
-input.close();
-in.close();
-}
+
     // read key parameters from a file and generate the private key
     public static PrivateKey readPrivKeyFromFile() throws IOException {
 
@@ -154,26 +130,15 @@ in.close();
             oin.close();
         }
     }
-/*
-    public static void readSecretKeyFromFile(){
-        InputStream in =
-                new FileInputStream("./KeyGen/AESSecret.key");
-        try {
-            byte[] privateKey = in.readAllBytes();
 
-            System.out.println(
-                    "Read from AESSecret.key: " + privateKey.toString() + "\n");
-
-            KeySpec keySpec = new KeySpec();
-            KeyFactory factory = KeyFactory.getInstance("AES");
-            Key key = factory.;
-
-            return key;
+    public static Key readSecretKeyFromFile() {
+        byte[] storedkey = new byte[128];
+        try (FileInputStream reader = new FileInputStream("./KeyGen/AESSecret.key")) {
+            reader.read(storedkey);
         } catch (Exception e) {
-            throw new RuntimeException("Spurious serialisation error", e);
-        } finally {
-            oin.close();
+            throw new RuntimeException("Ya done messed up!", e);
         }
+        Key symKey = new SecretKeySpec(storedkey, "AES");
+        return symKey;
     }
-*/
 }
